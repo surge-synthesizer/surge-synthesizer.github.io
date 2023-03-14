d40f9f9f3c81b5fb7c1ddf174c2742de0f544950
Mike Thierman <mthierman@gmail.com>
2023-03-14 11:53:30 -0600

    Fix TOC hamburger when resizing

--- scripts/manual.js
+++ scripts/manual.js
@@ -20,11 +20,13 @@ size.onchange = (e) => {
     if (e.matches) {
         document.getElementById("toc").style.display = "none";
         document.getElementById("toggle").style.display = "initial";
+        document.getElementById("toggle").innerHTML = "☰";
         document.getElementById("toc").addEventListener("click", toggleToc);
     }
     if (!e.matches) {
         document.getElementById("toc").removeEventListener("click", toggleToc);
         document.getElementById("toc").style.display = "initial";
+        document.getElementById("toggle").innerHTML = "☰";
         document.getElementById("toggle").style.display = "none";
     }
 };
