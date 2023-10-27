4e48c9be17813d0df2afdb554703b8052dd44ebe
mthierman <mthierman@gmail.com>
2023-10-27 13:12:45 -0600

    Revert images, add links to headers in manual

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,28 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import remarkToc from "remark-toc";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
     markdown: {
+        remarkPlugins: [remarkToc],
+        rehypePlugins: [
+            rehypeHeadingIds,
+            rehypeSlug,
+            [
+                rehypeAutolinkHeadings,
+                {
+                    behavior: "wrap",
+                    properties: {
+                        className: `no-underline`,
+                    },
+                },
+            ],
+        ],
         shikiConfig: {
             theme: "css-variables",
             wrap: true,
