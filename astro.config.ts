29a71a37737f8b77172556abbfb3070ab0a70f28
mthierman <mthierman@gmail.com>
2023-10-30 12:36:39 -0600

    Disable auto linked headings until we can solve ID clobbering

--- astro.config.ts
+++ astro.config.ts
@@ -2,27 +2,29 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import remarkToc from "remark-toc";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
+// Disable auto linking for headings until we can solve ID clobbering..
+// import remarkToc from "remark-toc";
+// import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+// import rehypeSlug from "rehype-slug";
+// import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
     markdown: {
-        remarkPlugins: [remarkToc],
+        // remarkPlugins: [remarkToc],
         rehypePlugins: [
-            rehypeHeadingIds,
-            rehypeSlug,
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: "wrap",
-                    properties: {
-                        className: `no-underline`,
-                    },
-                },
-            ],
+            // rehypeHeadingIds,
+            // [rehypeSlug, { prefix: Math.random() }],
+            // [
+            //     rehypeAutolinkHeadings,
+            //     {
+            //         behavior: "wrap",
+            //         properties: {
+            //             className: `no-underline`,
+            //         },
+            //     },
+            // ],
         ],
         shikiConfig: {
             theme: "css-variables",
