77aabfd582191c24ceb27d7ae29f18d76718120f
mthierman <mthierman@gmail.com>
2024-01-21 12:13:04 -0700

    Update & pin dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    plugins: [require("@tailwindcss/typography")],
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
         fontFamily: {
