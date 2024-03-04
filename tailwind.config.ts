cb19bf739565a3e9d4d3b6c709a24ae10095d3ae
mthierman <mthierman@gmail.com>
2024-03-03 19:57:42 -0700

    Update packages & GitHub actions, remove unused packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
     theme: {
         fontFamily: {
             sans: ["Lato"],
