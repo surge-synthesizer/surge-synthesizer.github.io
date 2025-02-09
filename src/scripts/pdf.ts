56ccb08d068e0715ab43dd649363e4b3cbe41e22
Mike Thierman <mthierman@gmail.com>
2025-02-09 07:00:42 -0700

    Remove path aliases, delete tsx dependency (#385)

--- src/scripts/pdf.ts
+++ src/scripts/pdf.ts
@@ -13,23 +13,25 @@ import puppeteer from "puppeteer";
         ? "https://surge-synthesizer.github.io/manual-xt/"
         : "http://localhost:4321/manual-xt/";
 
-    await page.goto(url, { waitUntil: "networkidle0" });
+    await mkdir(outdir, { recursive: true });
 
-    mkdir(outdir, { recursive: true })
-        .then(async () => {
-            await page.pdf({
-                path: resolve(outdir, "Surge-XT-Manual.pdf"),
-                margin: { top: "32px", bottom: "64px", left: "32px", right: "32px" },
-                printBackground: true,
-                displayHeaderFooter: true,
-                headerTemplate: "<div></div>",
-                footerTemplate: `<div style='border-top: 1px solid #D4D4D4; width: 100%; margin: 0 256px; text-align: center; font-size: 16px; padding-top: 16px;'>
+    try {
+        await page.goto(url, { waitUntil: "networkidle0" });
+
+        await page.pdf({
+            path: resolve(outdir, "Surge-XT-Manual.pdf"),
+            margin: { top: "32px", bottom: "64px", left: "32px", right: "32px" },
+            printBackground: true,
+            displayHeaderFooter: true,
+            headerTemplate: "<div></div>",
+            footerTemplate: `<div style='border-top: 1px solid #D4D4D4; width: 100%; margin: 0 256px; text-align: center; font-size: 16px; padding-top: 16px;'>
                         <span class='pageNumber'></span> <span>/</span> <span class='totalPages'></span>
                     </div>`,
-                format: "A4",
-            });
+            format: "A4",
+        });
 
-            await browser.close();
-        })
-        .catch(() => process.exit(1));
+        await browser.close();
+    } catch {
+        process.exit(1);
+    }
 })();
