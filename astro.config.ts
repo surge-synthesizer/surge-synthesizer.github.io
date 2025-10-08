d87dca30961a61c2f6bfbd807dde88296cf5acb3
mthierman <mthierman@gmail.com>
2025-10-07 19:13:25 -0600

    Add remark-github

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,16 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import remarkGithub from "remark-github";
 
 // https://astro.build/config
 export default defineConfig({
     vite: { plugins: [tailwindcss()] },
     integrations: [mdx(), react()],
     markdown: {
+        remarkPlugins: [
+            [remarkGithub, { repository: "https://github.com/surge-synthesizer/surge" }],
+        ],
         shikiConfig: {
             themes: {
                 light: "dark-plus",
