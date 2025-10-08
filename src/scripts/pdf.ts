c697f222fff8a361e97173e39d8a9dea4cf4c209
mthierman <mthierman@gmail.com>
2025-10-07 20:13:02 -0600

    Make PDF print properly on Windows

--- src/scripts/pdf.ts
+++ src/scripts/pdf.ts
@@ -1,11 +1,15 @@
 import { mkdir } from "node:fs/promises";
-import { dirname, resolve } from "node:path";
+import { dirname, join, resolve } from "node:path";
 import puppeteer from "puppeteer";
 
 (async () => {
     const outdir = resolve(dirname(import.meta.dirname), "../", "dist");
+    const outfile = resolve(outdir, "Surge-XT-Manual.pdf");
+
     const browser = await puppeteer.launch();
-    console.log(await browser.version());
+    const browser_version = await browser.version();
+
+    console.log(`${browser_version}: Printing PDF file to ${outfile}`);
 
     const page = await browser.newPage();
 
@@ -19,15 +23,10 @@ import puppeteer from "puppeteer";
         await page.goto(url, { waitUntil: "networkidle0" });
 
         await page.pdf({
-            path: resolve(outdir, "Surge-XT-Manual.pdf"),
-            margin: { top: "32px", bottom: "64px", left: "32px", right: "32px" },
-            printBackground: true,
-            displayHeaderFooter: true,
-            headerTemplate: "<div></div>",
-            footerTemplate: `<div style='border-top: 1px solid #D4D4D4; width: 100%; margin: 0 256px; text-align: center; font-size: 16px; padding-top: 16px;'>
-                        <span class='pageNumber'></span> <span>/</span> <span class='totalPages'></span>
-                    </div>`,
+            path: outfile,
             format: "A4",
+            margin: { top: "32px", bottom: "64px", left: "32px", right: "32px" },
+            displayHeaderFooter: true,
         });
 
         await browser.close();
