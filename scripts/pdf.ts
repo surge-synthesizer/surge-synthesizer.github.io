20292ce7b28cd36eec823ffb703aa4ba27e7ece8
mthierman <mthierman@gmail.com>
2025-10-07 22:44:11 -0600

    Move PDF script

--- scripts/pdf.ts
+++ scripts/pdf.ts
@@ -3,32 +3,27 @@ import { dirname, resolve } from "node:path";
 import puppeteer from "puppeteer";
 
 (async () => {
-    const outdir = resolve(dirname(import.meta.dirname), "../", "dist");
-    const outfile = resolve(outdir, "Surge-XT-Manual.pdf");
+    const dist = resolve(dirname(import.meta.dirname), `dist`);
+    const filename = resolve(dist, `Surge-XT-Manual.pdf`);
+
+    await mkdir(dist, { recursive: true });
 
     const browser = await puppeteer.launch();
     const browser_version = await browser.version();
 
-    console.log(`${browser_version}: Printing PDF file to ${outfile}`);
+    console.log(`${browser_version}: Printing PDF file to ${filename}`);
 
     const page = await browser.newPage();
-
-    const url = process.env.CI
-        ? "https://surge-synthesizer.github.io/manual-xt/"
-        : "http://localhost:4321/manual-xt/";
-
-    await mkdir(outdir, { recursive: true });
+    const url = `http://localhost:4321/manual-xt/`;
 
     try {
         await page.goto(url, { waitUntil: "networkidle0" });
-
         await page.pdf({
-            path: outfile,
+            path: filename,
             format: "A4",
             margin: { top: "32px", bottom: "64px", left: "32px", right: "32px" },
             displayHeaderFooter: true,
         });
-
         await browser.close();
     } catch {
         process.exit(1);
