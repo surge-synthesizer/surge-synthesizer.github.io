808e8d43891c75373522228f68719ff2b39dd7eb
mthierman <mthierman@gmail.com>
2025-10-09 23:13:43 -0600

    Remove react

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import remarkGithub from "remark-github";
@@ -7,7 +6,7 @@ import remarkGithub from "remark-github";
 // https://astro.build/config
 export default defineConfig({
     vite: { plugins: [tailwindcss()] },
-    integrations: [mdx(), react()],
+    integrations: [mdx()],
     markdown: {
         remarkPlugins: [
             [remarkGithub, { repository: "https://github.com/surge-synthesizer/surge" }],
